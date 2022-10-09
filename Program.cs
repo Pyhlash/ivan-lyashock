a4ca9f75d3d474252c643f60658652e0a0e32b54
Ivan lyashok <97744261+Pyhlash@users.noreply.github.com>
2022-10-09 21:35:23 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите a");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите b");
+        int b = int.Parse(Console.ReadLine());
+        a = (a % b) * (b % a) + 1;
+        Console.WriteLine(a);
+    }
+}
